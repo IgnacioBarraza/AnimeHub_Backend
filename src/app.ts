3cbe5af9eb735375f9b75c50237a3f63247fc8ed
Ignacio Barraza Rioja <ignacio.barraza.rioja@gmail.com>
2024-11-09 18:24:54 -0300

    ✨ Add security to the backend

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import helmet from 'helmet'
 import cors from 'cors'
 import bodyParser from 'body-parser'
 import cookieParser from 'cookie-parser'
+import ExpressMongoSanitize from 'express-mongo-sanitize'
 
 /** Middlewares imports **/
 import { notFound, errorHandler } from './middleware/index.middleware'
@@ -22,6 +23,18 @@ app.use(cors())
 app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: true, limit: '30mb' }))
 app.use(cookieParser())
+app.use(ExpressMongoSanitize())
+app.use(helmet())
+app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }))
+app.use(helmet.xssFilter())
+app.use(helmet.contentSecurityPolicy({
+  directives: {
+    defaultSrc: ["'self'"],
+    scriptSrc: ["'self'", "'trusted-cdn.com'"],
+    objectSrc: ["'none'"],
+    upgradeInsecureRequests: []
+  }
+}))
 
 /** APIs **/
 app.use('/api/',api)

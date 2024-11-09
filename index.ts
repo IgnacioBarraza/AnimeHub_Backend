755e1d1068b1c58526e6f168cc21dc9466685c6b
Ignacio Barraza Rioja <ignacio.barraza.rioja@gmail.com>
2024-11-09 16:57:23 -0300

    ✨ NorFound, ErrorHandler middlewares and api endpoint '/'

---
+++ index.ts
@@ -0,0 +1,10 @@
+import app from './src/app'
+
+
+const port = process.env.PORT || 5000
+
+app.listen(port, () => {
+  console.log(`Listening on port: ${port}`)
+  console.log(`http://localhost:${port}`)
+  console.log('AnimeHub Backend 🚀')
+})
---
+++ src/app.ts
@@ -0,0 +1,32 @@
+import { config } from 'dotenv'
+import express from 'express'
+import morgan from 'morgan'
+import helmet from 'helmet'
+import cors from 'cors'
+
+/** Middlewares imports **/
+import { notFound, errorHandler } from './middleware/index.middleware'
+
+/** APIs imports **/
+import api from './api/index.api'
+
+config()
+
+const app = express()
+
+app.use(morgan("dev"))
+app.use(helmet())
+app.use(cors())
+app.use(express.json())
+
+/** APIs **/
+app.use('/api/',api)
+
+/** Middlewares **/
+app.use(notFound)
+app.use(errorHandler)
+
+
+
+
+export default app
